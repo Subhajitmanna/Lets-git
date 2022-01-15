46607611a1ac816b6fca60035e839b07e002720d
subhajit manna <subhajitmanna0045@gmail.com>
2022-01-15 17:35:37 +0530

    initial commit3

--- Sum.java
+++ Sum.java
@@ -1,8 +1,10 @@
+package Ltsgit;
+
 public class Sum {
     public static void main(String[] args) {
         int a=8;
         int b=9;
         int c=a+b;
-        System.out.println(c);
+        System.out.println("the sum is: "+c);
     }
 }
