4a5e62908c00b507a353ae337df75a0997ac401a
subhajit manna <subhajitmanna0045@gmail.com>
2022-01-15 17:51:01 +0530

    multiply commit

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package Ltsgit;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a=9;
+        int b=4;
+        int c=a*b;
+        System.out.println("the multiply is: "+c);
+    }
+}
