bca05d2e8cc0fdd2bf610033d7b4ae25523b0965
subhajit manna <subhajitmanna0045@gmail.com>
2022-01-15 17:44:17 +0530

    initial commit4

--- diff.java
+++ diff.java
@@ -2,6 +2,9 @@ package Ltsgit;
 
 public class diff {
     public static void main(String[] args) {
-        
+        int a=8;
+        int b=9;
+        int c=a-b;
+        System.out.println("the difference is: "+c);
     }
 }
