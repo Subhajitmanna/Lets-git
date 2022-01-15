5e5464d49d9882327e16a1a32bdbfaa8deca2667
subhajit manna <subhajitmanna0045@gmail.com>
2022-01-15 19:31:51 +0530

    Divide commit

---
+++ Div.java
@@ -0,0 +1,10 @@
+package Ltsgit;
+
+public class Div {
+    public static void main(String[] args) {
+        int a=39;
+        int b=5;
+        int c=a/b;
+        System.out.println("the divide is: "+c);
+    }
+}
